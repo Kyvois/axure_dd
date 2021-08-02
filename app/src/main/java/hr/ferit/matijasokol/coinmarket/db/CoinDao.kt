03a88128eb5048f7e806657e1e7a75a63a0b8b76
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-08-02 16:18:06 +0000

    fas

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/db/CoinDao.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/db/CoinDao.kt
@@ -1,4 +1,12 @@
 package hr.ferit.matijasokol.coinmarket.db
 
 import androidx.room.Dao
-im
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import hr.ferit.matijasokol.coinmarket.models.Coin
+
+@Dao
+interface CoinDao {
+
+    @Inse
