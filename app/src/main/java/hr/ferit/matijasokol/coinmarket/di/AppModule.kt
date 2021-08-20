6fdcf8c39b6df57d71ef4ecbf82d3c5d4d15d5c6
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-08-20 11:33:48 +0000

    Opps

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/di/AppModule.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/di/AppModule.kt
@@ -5,4 +5,7 @@ import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import hr.ferit.matijasokol.coinmarket.db.CoinDatabase
+import hr.ferit.matijasokol.coinmarket.networking.CoinsM
