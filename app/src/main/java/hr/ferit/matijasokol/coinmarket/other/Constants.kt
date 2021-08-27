7e756450becd3e2a6dc591180cd7ce95499f0791
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-08-27 21:24:06 +0000

    Fixing layout

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/other/Constants.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/other/Constants.kt
@@ -1,1 +1,9 @@
-package hr.ferit.m
+package hr.ferit.matijasokol.coinmarket.other
+
+object Constants {
+
+    const val BASE_URL = "https://api.coingecko.com/api/v3/"
+
+    const val WEEK_LENGTH = 7
+
+    const val CHART_ANI
