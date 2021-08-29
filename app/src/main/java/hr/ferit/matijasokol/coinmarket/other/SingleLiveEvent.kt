280cff61bfac3c60c6532c3b2f816465d4dd91d8
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-08-29 11:53:39 +0000

    top level:

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/other/SingleLiveEvent.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/other/SingleLiveEvent.kt
@@ -1,1 +1,7 @@
-pack
+package hr.ferit.matijasokol.coinmarket.other
+
+/*
+ *  Copyright 2017 Google Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this fil
