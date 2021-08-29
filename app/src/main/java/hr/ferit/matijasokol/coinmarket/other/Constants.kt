5d2335a364f4c0aff9877f59d8c356a8ef34ebdc
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-08-29 05:40:17 +0000

    Create projects.md

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/other/Constants.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/other/Constants.kt
@@ -6,4 +6,9 @@ object Constants {
 
     const val WEEK_LENGTH = 7
 
-    const val CHART_ANI
+    const val CHART_ANIM_DURATION = 1500
+
+    const val RECYCLER_COLUMNS_NUMBER = 2
+
+    const val DB_NAME = "coin_db.db"
+}
