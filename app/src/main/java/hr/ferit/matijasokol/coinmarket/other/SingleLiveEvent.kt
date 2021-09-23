5abcfdd9749ae192d181911a69dfa303f8cf5ae7
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-09-23 08:22:10 +0000

    GPL License

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/other/SingleLiveEvent.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/other/SingleLiveEvent.kt
@@ -11,4 +11,10 @@ package hr.ferit.matijasokol.coinmarket.other
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, eith
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import android.util.Log
+import androidx.annotatio
