a58deeec2165ed86ad50093f8dbbeefb2f72cc99
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-11-19 11:41:50 +0000

    Missing files.

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/repository/CoinMarketRepository.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/repository/CoinMarketRepository.kt
@@ -1,4 +1,10 @@
 package hr.ferit.matijasokol.coinmarket.repository
 
 import hr.ferit.matijasokol.coinmarket.db.CoinDao
-import hr.ferit.mati
+import hr.ferit.matijasokol.coinmarket.models.Coin
+import hr.ferit.matijasokol.coinmarket.networking.CoinsMarketApi
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class
