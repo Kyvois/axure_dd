50660b1f01775e766aa17a9c1fdddb5f0f8266dd
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-11-30 20:35:11 +0000

    Small code improvements.

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/ui/coins/CoinsViewModel.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/ui/coins/CoinsViewModel.kt
@@ -5,4 +5,7 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-impor
+import androidx.lifecycle.viewModelScope
+import hr.ferit.matijasokol.coinmarket.R
+import hr.ferit.matijasokol.coinmarket.app.CoinMarketApplication
+import hr.ferit.matijasokol.coinmarket.model
