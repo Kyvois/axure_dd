0e39c2cb77a4536e124c1f6d6388df4e684d07c9
Kyvois <98109484+Kyvois@users.noreply.github.com>
2021-12-30 22:15:53 +0000

    fix favicon

--- app/src/main/java/hr/ferit/matijasokol/coinmarket/ui/fragments/coins/CoinsFragment.kt
+++ app/src/main/java/hr/ferit/matijasokol/coinmarket/ui/fragments/coins/CoinsFragment.kt
@@ -1,3 +1,8 @@
 package hr.ferit.matijasokol.coinmarket.ui.fragments.coins
 
-import and
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx
